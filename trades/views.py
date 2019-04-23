00bc20a2a2a6d080b26e65f204af3136a2dd8cb4
Jérôme Braure <braure.jerome@gmail.com>
2019-04-23 22:11:06 +0200

    creates HomeView

--- trades/views.py
+++ trades/views.py
@@ -1,16 +1,18 @@
 # from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-# from django.urls import reverse
+from django.urls import reverse
 from django.views import generic
-# from django.utils import timezone
+from django.utils import timezone
 
 from .models import Trade
 
 
-class IndexView(generic.ListView):
-    template_name = 'trades/index.html'
+class HomeView(generic.ListView):
+    template_name = 'trades/home.html'
     context_object_name = 'trades_list'
 
-    def get_queryset(self):
-        """Return the trades"""
-        return Trade.objects.all()
+    # @method_decorator(
+    #     login_required(login_url=reverse_lazy('login')))
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
