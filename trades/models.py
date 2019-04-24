c032f1320b399bab6fda0e256f46b37b478ccd22
Jérôme Braure <braure.jerome@gmail.com>
2019-04-24 12:30:33 +0200

    sets sell_date as nullable

--- trades/models.py
+++ trades/models.py
@@ -8,7 +8,7 @@ class Trade(models.Model):
     ticker = models.CharField('Ticker', max_length=20)
     currency = models.CharField('Currency', max_length=5)
     purchase_date = models.DateTimeField('Date purchased', default=timezone.now)
-    sell_date = models.DateTimeField('Date sold', auto_now=False)
+    sell_date = models.DateTimeField('Date sold', auto_now=False, default=None, blank=True, null=True)
     number_of_shares = models.IntegerField('Number of shares', default=1)
     price = models.FloatField()
 
