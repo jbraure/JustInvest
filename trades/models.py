defbfb233150529b85cde5a09150cebec489c34a
Jérôme Braure <braure.jerome@gmail.com>
2019-04-26 07:00:13 +0200

    adds default USD currency

--- trades/models.py
+++ trades/models.py
@@ -8,7 +8,7 @@ class Trade(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, default='0')
     ticker = models.CharField('Ticker', max_length=20)
-    currency = models.CharField('Currency', max_length=5)
+    currency = models.CharField('Currency', max_length=5, default='USD')
     purchase_date = models.DateTimeField('Date purchased', default=timezone.now)
     sell_date = models.DateTimeField('Date sold', auto_now=False, default=None, blank=True, null=True)
     number_of_shares = models.IntegerField('Number of shares', default=1)
