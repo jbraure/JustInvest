0979faef7307f3e1447a9b0451b9bb59919225c4
Jérôme Braure <braure.jerome@gmail.com>
2019-05-03 06:40:05 +0200

    changes ALLOWED_HOSTS

--- lsltrading/settings.py
+++ lsltrading/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'hbgf8v*xtb^46dlzgfrgra&c_rl5!0abib7gl)^0t#3rq)i#cw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lsltrader.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['justinvest.herokuapp.com', 'localhost']
 
 
 # Application definition
