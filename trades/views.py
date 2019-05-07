16035252d137ec67056990b367a4b79ff456e2f6
Jérôme Braure <braure.jerome@gmail.com>
2019-05-07 12:22:29 +0200

    formatting

--- trades/views.py
+++ trades/views.py
@@ -22,8 +22,7 @@ from pandas_datareader.data import DataReader
 class HomeView(TemplateView):
     template_name = 'trades/home.html'
 
-    @method_decorator(
-        login_required(login_url=reverse_lazy('login')))
+    @method_decorator(login_required(login_url=reverse_lazy('login')))
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         return self.render_to_response(context)
