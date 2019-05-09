ee38ff13fbc920ae5cb56054d5bb7d57c7b285f7
Jérôme Braure <braure.jerome@gmail.com>
2019-05-09 07:05:18 +0200

    renames a function and adds docstring

--- trades/lib/justinvest/finance.py
+++ trades/lib/justinvest/finance.py
@@ -18,24 +18,33 @@ def is_ticker_existing(ticker):
     except:
         return False
 
-def index_to_unix(row):
+def date_to_unix_timestamp(row):
+    """
+    Takes a Pandas dataframe row and converts the data in the 'unix'
+    column from Pandas Timestamp to a timestamp in unix format,
+    ex : 1556890200000
+    """
     row.unix = row.unix.value // 10**6
     return row
 
 def get_ohlc_quote(ticker, start_year=2017):
     """
-    Returns a list of quotes (Date, Open, High, Low, Close) values
+    Returns a List of quotes data list [Date, Open, High, Low, Close] values
     with a date in unix timestamp format.
     """
     start_date = date(start_year,1,1)
     stock_data_df = DataReader(ticker, 'yahoo', start=start_date)
     # add a column in unix timestamp format
     stock_data_df['unix'] = stock_data_df.index
-    stock_data_df = stock_data_df.apply(index_to_unix, axis='columns')
+    stock_data_df = stock_data_df.apply(date_to_unix_timestamp, axis='columns')
     values_list = stock_data_df.loc[:,['unix', 'Open', 'High', 'Low', 'Close', 'Volume']].values.tolist()
     return values_list
 
 def get_last_close(ticker):
+    """
+    Returns the last Stockmarket Close value for the specified ticker
+    as a float.
+    """
     start_date = date.today() - timedelta(4)
     stock_data = DataReader(ticker, 'yahoo', start=start_date)
     last_close = float(stock_data.tail(1)['Close'])
