c0b5614460cd1fb473615db99e8c9c0a0085dfbd
Jérôme Braure <braure.jerome@gmail.com>
2019-05-10 22:12:17 +0200

    adds ticker validation in Trade creation form

--- trades/forms.py
+++ trades/forms.py
@@ -1,7 +1,16 @@
-from django.forms import ModelForm
+
+from django.forms import ModelForm, ValidationError
+
 from .models import Trade
+from .lib.justinvest import finance
 
 class TradeForm(ModelForm):
+    def clean_ticker(self):
+        ticker_data = self.cleaned_data['ticker']
+        if not finance.is_ticker_existing(ticker_data):
+            raise ValidationError('This ticker could not be found in Yahoo finance!')
+        return ticker_data
+
     class Meta:
         model = Trade
         fields = ['ticker', 'name', 'asset_class', 'currency', 'trade_date', \
